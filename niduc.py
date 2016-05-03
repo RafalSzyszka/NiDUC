f964044c9e88f5aec33f7ef5cdd72460b67cce0a
Rafal <rafal.sz.49@gmail.com>
2016-05-03 15:20:20 +0200

    Last stable

--- niduc.py
+++ niduc.py
@@ -3,53 +3,55 @@ from arqmodel import ARQModel
 from protocols import *
 from noise import NoiseGenerator
 
-def printProgramParams():
-	print("\n\t###################-PARAMETRY PROGRAMU-###################")
-	print("\t#--rfp = "+str(rfp)+"\tZakres losowania liczb dla pakietu-------#")
-	print("\t#--rfb = "+str(rfb)+"\tZakres losowania liczb dla bitu----------#")
-	print("\t#--rfs = "+str(rfs)+"\tZakres losowania liczb dla bitow CRC-----#")
-	print("\t#--pfp = "+str(pfp)+"\tDeterminuje P(zaklocenie pakietu)--------#")
-	print("\t#--pfb = "+str(pfb)+"\tDeterminuje P(zaklocenie bitu)-----------#")
-	print("\t#--pfs = "+str(pfs)+"\tDeterminuje P(zaklocenie bitu CRC)-------#")
-	print("\t#--bytes = "+str(bytes)+"\tIlosc bajtow w pakietach-----------------#")
-	print("\t##########################################################\n\n")
 
-#--------------------------------PARAMETRY PROGRAMU--------------------------------#
-#-------------------------PARAMETRY GENERATORA SZUMOW-------------------------#
-rfp = 1000		#if rand(0, rfp) % pfp == 0														#
-rfb = 100			#if rand(0, rfb) % pfb == 0														#
-rfs = 100			#if rand(0, rfs) % pfs == 0															#
-pfp = 200			#~2% na paczke																		#
-pfb = 10			#~10% na bit																				#
-pfs = 20			#~5% na bity kontrolne
-rgw = 1000                      #if rand(0, rgw) % pgw == 0	
-rgg = 100                       #if rand(0, rgg) % pgg == 0	
-pgw = 10                       #~1% na rozpoczecie serii bledow
-pgg = 10                        #~10% na zakonczenie serii bledow
-toc = 0                        #typ wykorzystywanego kanalu 0 -BSC, 1-Gilberta, 2-BEC
-#-------------------------------------POZOSTALE-----------------------------------------#																														#
-bytes = 64		#ilosc bajtow w paczce																#
-errors = 0			#ilosc bledow podczas transmisji													#		
-packages = 0		#ilosc przesylanych pakietow														#
-percent = 0		#procent bledow																		#
-#--------------------------------PARAMETRY PROGRAMU--------------------------------#
+def printProgramParams():
+    print("\n\t###################-PARAMETRY PROGRAMU-###################")
+    print("\t#--rfp = " + str(rfp) + "\tZakres losowania liczb dla pakietu-------#")
+    print("\t#--rfb = " + str(rfb) + "\tZakres losowania liczb dla bitu----------#")
+    print("\t#--rfs = " + str(rfs) + "\tZakres losowania liczb dla bitow CRC-----#")
+    print("\t#--pfp = " + str(pfp) + "\tDeterminuje P(zaklocenie pakietu)--------#")
+    print("\t#--pfb = " + str(pfb) + "\tDeterminuje P(zaklocenie bitu)-----------#")
+    print("\t#--pfs = " + str(pfs) + "\tDeterminuje P(zaklocenie bitu CRC)-------#")
+    print("\t#--bytes = " + str(bytes) + "\tIlosc bajtow w pakietach-----------------#")
+    print("\t##########################################################\n\n")
+
+
+# --------------------------------PARAMETRY PROGRAMU--------------------------------#
+# -------------------------PARAMETRY GENERATORA SZUMOW-------------------------#
+rfp = 1000  # if rand(0, rfp) % pfp == 0														#
+rfb = 100  # if rand(0, rfb) % pfb == 0														#
+rfs = 100  # if rand(0, rfs) % pfs == 0															#
+pfp = 200  # ~2% na paczke																		#
+pfb = 10  # ~10% na bit																				#
+pfs = 20  # ~5% na bity kontrolne
+rgw = 1000  # if rand(0, rgw) % pgw == 0
+rgg = 100  # if rand(0, rgg) % pgg == 0
+pgw = 10  # ~1% na rozpoczecie serii bledow
+pgg = 10  # ~10% na zakonczenie serii bledow
+toc = 0  # typ wykorzystywanego kanalu 0 -BSC, 1-Gilberta, 2-BEC
+# -------------------------------------POZOSTALE-----------------------------------------#																														#
+bytes = 64  # ilosc bajtow w paczce																#
+errors = 0  # ilosc bledow podczas transmisji													#
+packages = 0  # ilosc przesylanych pakietow														#
+percent = 0  # procent bledow																		#
+# --------------------------------PARAMETRY PROGRAMU--------------------------------#
 
 print("\n#-----------------------SYMULACJA-----------------------#\n")
 
 printProgramParams()
 
-#inicjalizacja dekoderow ARQ		
-sourceARQ = ARQModel()	#zrodlowy ARQ
-destARQ = ARQModel()	#docelowy ARQ
+# inicjalizacja dekoderow ARQ
+sourceARQ = ARQModel()  # zrodlowy ARQ
+destARQ = ARQModel()  # docelowy ARQ
 noiseGenerator = NoiseGenerator(rfp, rfb, rfs, pfp, pfb, pfs, rgw, rgg, pgw, pgg, toc)
 sawProtocol = SAWProtocol(sourceARQ, destARQ, noiseGenerator, bytes)
 
 sawProtocol.prepareDecoders('wave.wav')
 sawProtocol.transmit()
 
-errors = sawProtocol.errors		#pobranie ilosci bledow
-packages = len(sawProtocol.sourceARQ.packages) + 0.0		#pobranie liczby wyslanych pakietow
-percent = str( (errors/packages)*100) + '%'		#wyliczenie procentu bledow
+errors = sawProtocol.errors  # pobranie ilosci bledow
+packages = len(sawProtocol.sourceARQ.packages) + 0.0  # pobranie liczby wyslanych pakietow
+percent = str((errors / packages) * 100) + '%'  # wyliczenie procentu bledow
 errors = str(errors) + '/' + str(len(sawProtocol.sourceARQ.packages))
 
 print("\n<ARQ>\t\tFile sended.\n\t\t\tErrors: ", errors, "\t" + percent)
@@ -60,18 +62,18 @@ print("\n#-----------------------SYMULACJA GBN-----------------------#\n")
 
 printProgramParams()
 
-#inicjalizacja dekoderow ARQ		
-sourceARQ = ARQModel()	#zrodlowy ARQ
-destARQ = ARQModel()	#docelowy ARQ
+# inicjalizacja dekoderow ARQ
+sourceARQ = ARQModel()  # zrodlowy ARQ
+destARQ = ARQModel()  # docelowy ARQ
 noiseGenerator = NoiseGenerator(rfp, rfb, rfs, pfp, pfb, pfs, rgw, rgg, pgw, pgg, toc)
 gobackProtocol = GoBackProtocol(sourceARQ, destARQ, noiseGenerator, bytes)
 
 gobackProtocol.prepareDecoders('wave.wav')
 gobackProtocol.transmit()
 
-errors = gobackProtocol.errors		#pobranie ilosci bledow
-packages = len(gobackProtocol.sourceARQ.packages) + 0.0		#pobranie liczby wyslanych pakietow
-percent = str( (errors/packages)*100) + '%'		#wyliczenie procentu bledow
+errors = gobackProtocol.errors  # pobranie ilosci bledow
+packages = len(gobackProtocol.sourceARQ.packages) + 0.0  # pobranie liczby wyslanych pakietow
+percent = str((errors / packages) * 100) + '%'  # wyliczenie procentu bledow
 errors = str(errors) + '/' + str(len(gobackProtocol.sourceARQ.packages))
 
 print("\n<ARQ>\t\tFile sended.\n\t\t\tErrors: ", errors, "\t" + percent)
--- noise.py
+++ noise.py
@@ -1,6 +1,6 @@
-#NOISE GENERATOR
-#Pobiera pakiet i z okreslonym modyfikowalnym prawdopodobienstwem dodaje do niego szum
-#-----Parametry:
+# NOISE GENERATOR
+# Pobiera pakiet i z okreslonym modyfikowalnym prawdopodobienstwem dodaje do niego szum
+# -----Parametry:
 #	rangeForPack
 #	rangeForBit
 #	rangeForSecure
@@ -10,150 +10,154 @@
 
 import random
 
+
 class NoiseGenerator:
-	
-	#Parametry determinujace prawdopodobienstwo wystapienia przeklamania
-	rangeForPack = 0
-	rangeForBit = 0
-	rangeForSecure = 0
-	probForPack = 0
-	probForBit = 0
-	probForSecure = 0
+    # Parametry determinujace prawdopodobienstwo wystapienia przeklamania
+    rangeForPack = 0
+    rangeForBit = 0
+    rangeForSecure = 0
+    probForPack = 0
+    probForBit = 0
+    probForSecure = 0
 
-	rangeGilbertWrong = 0
-	rangeGilbertGood = 0
-	probGilbertWrong = 0
-	probGilbertGood = 0
-	typeOfChannel = 0
-	noiseFlag = 0
-	
-	def __init__(self, rfp, rfb, rfs, pfp, pfb, pfs, rgw, rgg, pgw, pgg, toc):
-		self.rangeForPack = rfp
-		self.rangeForBit = rfb
-		self.rangeForSecure = rfs
-		self.probForPack = pfp
-		self.probForBit = pfb
-		self.probForSecure = pfs
-		
-                self.rangeGilbertWrong = rgw
-                self.rangeGilbertGood = rgg
-                self.probGilbertWrong = pgw
-                self.probGilbertGood = pgg
-                
-                self.typeOfChannel = toc
-	
-	def addNoise(self, pack):		#prymitywne zaszumienie pakietu
-                if(self.typeOfChannel == 0):
-                                        
-                        grain = random.randint(0, self.rangeForPack)		#wylosowanie liczby z przedzialu [0;rangeForPack)
-		
-                        #istnieje prawdopodobienstwo, ze pakiet zostanie zaklocony
-                        #jest ono determinowane przez parametr 'probForPack'
-                        if(grain % self.probForPack == 0):
-		
-                                damagedPack = [byte for byte in pack]	#przekopiowanie pakietu
-		
-                                ones = damagedPack.pop()		#pozbywamy sie bitow kontrolnych
-                                even = damagedPack.pop()		
-			
-                                #konwersja przeklamanego ciagu bitow na pakiet mozliwy do transmisji
-                                damagedPack = self.convertBitStringToPack(self.changeBits(damagedPack))
-			
-                                #przeklamanie bitow kontrolnych i zworcenie zakloconego pakietu
-                                return self.changeSecureBits(damagedPack, even, ones)
-			
-                        #istnieje duze prawdopodobienstwo ze pakiet nie zostanie zaklocony
-                        else:
-                                return pack
+    rangeGilbertWrong = 0
+    rangeGilbertGood = 0
+    probGilbertWrong = 0
+    probGilbertGood = 0
+    typeOfChannel = 0
+    noiseFlag = 0
+
+    def __init__(self, rfp, rfb, rfs, pfp, pfb, pfs, rgw, rgg, pgw, pgg, toc):
+        self.rangeForPack = rfp
+        self.rangeForBit = rfb
+        self.rangeForSecure = rfs
+        self.probForPack = pfp
+        self.probForBit = pfb
+        self.probForSecure = pfs
+
+        self.rangeGilbertWrong = rgw
+        self.rangeGilbertGood = rgg
+        self.probGilbertWrong = pgw
+        self.probGilbertGood = pgg
+
+        self.typeOfChannel = toc
+
+    def addNoise(self, pack):  # prymitywne zaszumienie pakietu
+        if (self.typeOfChannel == 0):
+
+            grain = random.randint(0, self.rangeForPack)  # wylosowanie liczby z przedzialu [0;rangeForPack)
+
+            # istnieje prawdopodobienstwo, ze pakiet zostanie zaklocony
+            # jest ono determinowane przez parametr 'probForPack'
+            if (grain % self.probForPack == 0):
+
+                damagedPack = [byte for byte in pack]  # przekopiowanie pakietu
+
+                ones = damagedPack.pop()  # pozbywamy sie bitow kontrolnych
+                even = damagedPack.pop()
+
+                # konwersja przeklamanego ciagu bitow na pakiet mozliwy do transmisji
+                damagedPack = self.convertBitStringToPack(self.changeBits(damagedPack))
+
+                # przeklamanie bitow kontrolnych i zworcenie zakloconego pakietu
+                return self.changeSecureBits(damagedPack, even, ones)
+
+            # istnieje duze prawdopodobienstwo ze pakiet nie zostanie zaklocony
+            else:
+                return pack
+        else:
+            if (self.typeOfChannel == 1):
+
+                if (self.noiseFlag == 1):
+                    if (random.randint(0,
+                                       self.rangeGilbertGood) % self.probGilbertGood == 0):  # prawdopodobienstwo rozpoczecia serii bledow
+                        self.noiseFlag = 0
+
+                        damagedPack = [byte for byte in pack]  # przekopiowanie pakietu
+                        ones = damagedPack.pop()  # pozbywamy sie bitow kontrolnych
+                        even = damagedPack.pop()
+
+                        # konwersja przeklamanego ciagu bitow na pakiet mozliwy do transmisji
+                        damagedPack = self.convertBitStringToPack(self.gilbertChannel(damagedPack))
+
+                        # przeklamanie bitow kontrolnych i zworcenie zakloconego pakietu
+                        return self.changeSecureBits(damagedPack, even, ones)
+
+                    # istnieje duze prawdopodobienstwo ze pakiet nie zostanie zaklocony
+                    else:
+                        if (random.randint(0,
+                                           self.rangeGilbertWrong) % self.probGilbertWrong == 0):  # prawdopodobienstwo rozpoczecia serii bledow
+                            self.noiseFlag = 1
+                        return pack
                 else:
-                        if(self.typeOfChannel == 1):
-		
-                                if(self.noiseFlag == 1):
-                                        if(random.randint(0, self.rangeGilbertGood) % self.probGilbertGood == 0) : #prawdopodobienstwo rozpoczecia serii bledow
-                                                self.noiseFlag = 0 
-		
-                                        damagedPack = [byte for byte in pack]	#przekopiowanie pakietu
-                                        ones = damagedPack.pop()		#pozbywamy sie bitow kontrolnych
-                                        even = damagedPack.pop()		
-			
-                                        #konwersja przeklamanego ciagu bitow na pakiet mozliwy do transmisji
-                                        damagedPack = self.convertBitStringToPack(self.gilbertChannel(damagedPack))
-			
-                                        #przeklamanie bitow kontrolnych i zworcenie zakloconego pakietu
-                                        return self.changeSecureBits(damagedPack, even, ones)
-			
-                                #istnieje duze prawdopodobienstwo ze pakiet nie zostanie zaklocony
-                                else:
-                                        if(random.randint(0, self.rangeGilbertWrong) % self.probGilbertWrong == 0) : #prawdopodobienstwo rozpoczecia serii bledow
-                                                self.noiseFlag = 1 
-                                        return pack
-                        else:
-                                return pack
-			
-	def convertBitStringToPack(self, string):		#konwertuje napis na pakiet
-		pack = []			#miejsce na pakiet
-		tmp = ''				#miejsce na pojedyncze bajty
-		for i in range(0, len(string)):		#proces wydzielania bajtow
-			if(i % 8 == 0 and i != 0):
-				pack.append(tmp)
-				tmp = ''
-			tmp += string[i]
-			
-			if(i == len(string)-1):
-				pack.append(tmp)
-		
-		return pack
-		
-	def changeBits(self, damagedPack):
-		string = ''		#przygotowanie miejsca na zaklocony pakiet
-		
-		for byte in damagedPack:		#wybieramy poszczegolne bajty z paczki
-			for bit in byte:					#wybieramy poszczegolne bity z kazdego bajtu
-				#istnieje prawdopodobienstwo, ze bit zostanie zmieniony
-				#dereminuja to parametry 'probForBit' oraz 'rangeForBit'
-				if(random.randint(0, self.rangeForBit) % self.probForBit == 0):
-					if(bit == '0'):
-						string += '1'		#zamiana 0->1
-					else:
-						string += '0'		#zamiana 1->0
-				else:
-					string += bit			#bez zmian
-		
-		return string	
+                    return pack
 
-	'''def gilbertChannel(self, damagedPack):
-		string = "" #przygotowanie miejsca na zaklocony pakiet
-                
-        for byte in damagedPack:		#wybieramy poszczegolne bajty z paczki
-			for bit in byte:
-                if(self.noiseFlag == 0):                               
-                    if(random.randint(0, self.rangeGilbertWrong) % self.probGilbertWrong == 0) : #prawdopodobienstwo rozpoczecia serii bledow
-                        self.noiseFlag = 1                #ustawienie flagi bledow seryjnych
-                        if(bit == '0'):
-                            string += '1'		#zamiana 0->1
+    def convertBitStringToPack(self, string):  # konwertuje napis na pakiet
+        pack = []  # miejsce na pakiet
+        tmp = ''  # miejsce na pojedyncze bajty
+        for i in range(0, len(string)):  # proces wydzielania bajtow
+            if (i % 8 == 0 and i != 0):
+                pack.append(tmp)
+                tmp = ''
+            tmp += string[i]
+
+            if (i == len(string) - 1):
+                pack.append(tmp)
+
+        return pack
+
+    def changeBits(self, damagedPack):
+        string = ''  # przygotowanie miejsca na zaklocony pakiet
+
+        for byte in damagedPack:  # wybieramy poszczegolne bajty z paczki
+            for bit in byte:  # wybieramy poszczegolne bity z kazdego bajtu
+                # istnieje prawdopodobienstwo, ze bit zostanie zmieniony
+                # dereminuja to parametry 'probForBit' oraz 'rangeForBit'
+                if (random.randint(0, self.rangeForBit) % self.probForBit == 0):
+                    if (bit == '0'):
+                        string += '1'  # zamiana 0->1
+                    else:
+                        string += '0'  # zamiana 1->0
+                else:
+                    string += bit  # bez zmian
+
+        return string
+
+    def gilbertChannel(self, damagedPack):
+        string = ''  # przygotowanie miejsca na zaklocony pakiet
+
+        for byte in damagedPack:  # wybieramy poszczegolne bajty z paczki
+            for bit in byte:
+                if (self.noiseFlag == 0):
+
+                    if (random.randint(0,
+                                       self.rangeGilbertWrong) % self.probGilbertWrong == 0):  # prawdopodobienstwo rozpoczecia serii bledow
+                        self.noiseFlag = 1  # ustawienie flagi bledow seryjnych
+                        if (bit == '0'):
+                            string += '1'  # zamiana 0->1
                         else:
-                             string += '0'		#zamiana 1->0
+                            string += '0'  # zamiana 1->0
                     else:
                         string += bit
                 else:
-                    if(random.randint(0, self.rangeGilbertGood) % self.probGilbertGood == 0) : #prawdopodobienstwo rozpoczecia serii bledow
-                        self.noiseFlag = 0                #ustawienie flagi bledow seryjnych
+                    if (random.randint(0,
+                                       self.rangeGilbertGood) % self.probGilbertGood == 0):  # prawdopodobienstwo rozpoczecia serii bledow
+                        self.noiseFlag = 0  # ustawienie flagi bledow seryjnych
                         string += bit
                     else:
-                        if(bit == '0'):
-                            string += '1'		#zamiana 0->1
+                        if (bit == '0'):
+                            string += '1'  # zamiana 0->1
                         else:
-                            string += '0'		#zamiana 1->0 
-                                
-      	return string'''
-		
-	def changeSecureBits(self, damagedPack, even, ones):
-		#istnieje prawdopodobienstwo, ze bity kontrolne zostana zmienione
-		#dereminuja to parametry 'probForSecure' oraz 'rangeForSecure'
-		if(random.randint(0, self.rangeForSecure) % self.probForSecure == 0):
-			even = random.randint(0,1)
-			ones = random.randint(0, 65536)
-				
-		damagedPack.append(even)
-		damagedPack.append(ones)
-		return damagedPack		#zwrocenie zakloconego pakietu
+                            string += '0'  # zamiana 1->0 
+        return string
+
+    def changeSecureBits(self, damagedPack, even, ones):
+        # istnieje prawdopodobienstwo, ze bity kontrolne zostana zmienione
+        # dereminuja to parametry 'probForSecure' oraz 'rangeForSecure'
+        if (random.randint(0, self.rangeForSecure) % self.probForSecure == 0):
+            even = random.randint(0, 1)
+            ones = random.randint(0, 65536)
+
+        damagedPack.append(even)
+        damagedPack.append(ones)
+        return damagedPack  # zwrocenie zakloconego pakietu
